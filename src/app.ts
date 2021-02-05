cc8c84891da013891d61c95b4903341e83c7255c
Wojciech Sromek <wojciech.sromek@codete.com>
2021-02-05 17:43:57 +0100

    Docker developers setup

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,8 @@ import express, {Request, Response} from 'express';
 const app: express.Application = express();
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!');
+    const name = "Wojtek";
+    res.send(`Hello ${name}!`);
 })
 
 app.listen(3000, () => console.log('API listening on port 3000'));
